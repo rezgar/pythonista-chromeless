9d38f26c18bb386ce9648d38c90992ff439988e9
Rezgar Cadro <rezgar.cadro@gmail.com>
2021-11-26 17:03:13 +0200

    Proxy syntax ix

--- server/server.py
+++ server/server.py
@@ -189,11 +189,12 @@ def get_default_chrome_options(self, dirname):
         options.add_argument('--proxy-server=socks5://127.0.0.1:9050')
     
     if self.proxy:
-        proxy_creds = f'{self.proxy["username"]}:{self.proxy["password"]}' if "username" in self.proxy else ""
-        proxy_uri = f'{self.proxy["host"]}:{self.proxy["port"]}'
+        proxy_creds = f'{self.proxy["username"]}:{self.proxy["password"]}@'
+        proxy_address = f'{self.proxy["host"]}:{self.proxy["port"]}'
+        proxy_uri = "https://" + ((proxy_creds + "@") if "username" in self.proxy else "") + proxy_address
 
         seleniumwire_options['proxy'] = {
-            'https': f'https://{proxy_creds}@{proxy_uri}',
+            'https': proxy_uri,
             'no_proxy': 'localhost,127.0.0.1'
         }
 
